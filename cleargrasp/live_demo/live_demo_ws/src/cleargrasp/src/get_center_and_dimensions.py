d5a98c48a3d9c943b84f096c7378002083da6e95
vlatkamihic <vlatka.mihic25@gmail.com>
2023-07-18 17:27:31 +0200

    Added node for 2. step - getting object center and dimensions

--- cleargrasp/live_demo/live_demo_ws/src/cleargrasp/src/get_center_and_dimensions.py
+++ cleargrasp/live_demo/live_demo_ws/src/cleargrasp/src/get_center_and_dimensions.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python3
+import sys
+import os
 import open3d as o3d
 import numpy as np
 import pyransac3d as pyrsc
 import cv2
+import glob
 from PIL import Image
+import rospy
+from cleargrasp.srv import CheckCurrentPhase
 
-FX_DEPTH = 597.9033203125
-FY_DEPTH = 598.47998046875
-CX_DEPTH = 323.8436584472656
-CY_DEPTH = 236.32774353027344
+isFinished = False
+isMyTurn =  False
+
 
 def display_inlier_outlier(cloud, ind):
     inlier_cloud = cloud.select_by_index(ind)
@@ -22,37 +27,17 @@ def display_inlier_outlier(cloud, ind):
                                       lookat=[2.6172, 2.0475, 1.532],
                                       up=[-0.0694, -0.9768, 0.2024])
 
-if __name__ == '__main__':
+def wait_for_live_demo():
+    global isFinished
+    global isMyTurn
+    
+    while(isMyTurn == False):
+        current_phase = rospy.ServiceProxy('check_current_phase', CheckCurrentPhase)
+        isMyTurn = (current_phase(1, isFinished)).isMyTurn
 
-    path = "/home/robot/cleargrasp/data/captures/exp-056/"
-
-    # pil_image = Image.open(path+"output-depth/000000000-output-depth-rgb.png")
-    # np_array = np.array(pil_image)
-    # open3d_image = o3d.geometry.Image(np_array.astype(np.uint8))
-    # # Convert the Open3D image to a compatible format
-    # open3d_image = o3d.create_compatible(o3d.geometry.RGBDImage)
-    # intrinsic = o3d.camera.PinholeCameraIntrinsic()
-
-    # width = 640
-    # height = 480
-    # fx = 597.9033203125
-    # fy = 598.47998046875
-    # cx = 323.8436584472656
-    # cy = 236.32774353027344
-
-    # intrinsic.set_intrinsics(width, height, fx, fy, cx, cy)
-    # point_cloud = o3d.geometry.PointCloud.create_from_depth_image(open3d_image, intrinsic, depth_scale=1000.0, depth_trunc=1000.0, stride=1)
-
-    # o3d.io.write_point_cloud(path+"my_point_cloud.ply", point_cloud)
+def get_object_center_and_dimensions(path):
 
     point_cloud_og = o3d.io.read_point_cloud(path + "output-point-cloud/output-point-cloud-object.ply")
-    # point_cloud = o3d.io.read_point_cloud(path + "output-point-cloud/copy_of_fragment.ply")
-    
-    # _, ind = point_cloud_og.remove_statistical_outlier(50, 7.0)
-    # display_inlier_outlier(point_cloud_og, ind)
-    # point_cloud_og = point_cloud_og.select_by_index(ind)
-    # # o3d.io.write_point_cloud(path + "output-point-cloud/copy_of_fragment.ply", point_cloud_og)
-    # bounding_box_og = point_cloud_og.get_axis_aligned_bounding_box()
 
     surface_ptc = o3d.io.read_point_cloud(path + "input-point-cloud/000000000-input-pointcloud.ply")
 
@@ -66,8 +51,7 @@ if __name__ == '__main__':
     unit_yaxis = yaxis / np.linalg.norm(yaxis)
     dot_product = np.dot(unit_normal, unit_yaxis)
     angle = -np.arccos(dot_product)
-    # angle = 1.57079632679
-    print(angle)
+
     rotation_matrix = np.matrix([[1, 0, 0],
                    [0, np.cos(angle),-np.sin(angle)],
                    [0, np.sin(angle), np.cos(angle)]])
@@ -78,14 +62,12 @@ if __name__ == '__main__':
     # Extract the dominant surface points
     dominant_surface = surface_ptc.select_by_index(inliers)
 
-    # # Get the x, y, and z coordinates of the dominant surface
+    # Get the x, y, and z coordinates of the dominant surface
     coordinates = np.asarray(dominant_surface.points)
-    # # Create an Open3D point cloud from the coordinates
+    # Create an Open3D point cloud from the coordinates
     dominant_surface_cloud = o3d.geometry.PointCloud()
     dominant_surface_cloud.points = o3d.utility.Vector3dVector(coordinates)
-    # Visualize the dominant surface
-    # o3d.visualization.draw_geometries([surface_ptc])
-    # o3d.visualization.draw_geometries([dominant_surface_cloud])
+
 
     _, ind = point_cloud_og.remove_statistical_outlier(30, 7.0)
     display_inlier_outlier(point_cloud_og, ind)
@@ -110,16 +92,34 @@ if __name__ == '__main__':
     print("Bounding box center:", center)
     print("Bounding box dimensions (L x W x H):", length, width, height)
 
-    # length, width, height = bounding_box_og.get_extent()
-    # center = bounding_box_og.get_center()
-
-    # print("Bounding box OG center:", center)
-    # print("Bounding box OG dimensions (L x W x H):", length, width, height)
-
     o3d.visualization.draw_geometries([bounding_box, point_cloud, point_cloud_2, dominant_surface, mesh, mesh_sphere, mesh_frame])
 
+    return center, length, width, height
 
+if __name__ == '__main__':
 
+    dir_path = "/home/robot/cleargrasp/data/captures/"
+
+    rospy.init_node('get_center_and_dimensions')
+    rospy.wait_for_service('check_current_phase')
+    wait_for_live_demo()
+
+    runs = sorted(glob.glob(os.path.join(dir_path, 'exp-*')))
+    prev_run_id = int(runs[-1].split('-')[-1]) if runs else 0
     
+    path = dir_path + "exp-0" + str(prev_run_id) + "/"
 
-    
+    center, length, width, height = get_object_center_and_dimensions(path)
+
+    result_folder_path = path + "result-center-and-dimensions/"
+    os.makedirs(result_folder_path)
+
+    center_file = open(result_folder_path + "center.txt", "w")
+    dimensions_file = open(result_folder_path + "dimensions.txt", "w")
+
+    center_file.write(str(center))
+    dimensions_file.write(str(length) + "\n" + str(width) + "\n" + str(height))
+
+    isFinished = True
+    current_phase = rospy.ServiceProxy('check_current_phase', CheckCurrentPhase)
+    isMyTurn = (current_phase(1, isFinished)).isMyTurn
